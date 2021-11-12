1000cb66beb8b895e7b7afd3d66cb1cb9433736f
iHuGi <70445157+iHuGi@users.noreply.github.com>
2021-11-12 16:18:23 +0000

    Added full project
    Added source files for the project.

---
+++ src/main/java/academy/learnprogramming/util/AttributeNames.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming.util;
+
+public final class AttributeNames {
+
+    // == constants ==
+
+    public static final String TODO_ITEM = "todoItem";
+
+    // == constructor ==
+    private AttributeNames() {
+    }
+}
